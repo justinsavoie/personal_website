15914cecb50954b9cad6c4f6649d107b8be4f5a7
justinsavoie <savoiejustin99@gmail.com>
2020-04-25 00:47:00 -0400

    Update cube_art.js

--- cube_art.js
+++ cube_art.js
@@ -2,7 +2,7 @@ let cubes = [];
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  for (let i = 0; i < 10; i++){
+  for (let i = 0; i < 30; i++){
     cubes.push(new cube(random(width),random(height),random(width/5),random(height/3),[random(255),random(255),random(255)]))
   }
 
