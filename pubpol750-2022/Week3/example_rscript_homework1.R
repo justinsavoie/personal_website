249f327d879fc926a840c2210b3013ed25f3be0e
justinsavoie <savoiejustin99@gmail.com>
2022-05-26 22:25:40 -0400

    ...

---
+++ pubpol750-2022/Week3/example_rscript_homework1.R
@@ -0,0 +1,9 @@
+# Create a R script like this one
+
+library(readxl)
+library(tidyverse)
+library(lubridate)
+
+Parliamentarians <- read_excel("~/Downloads/Parliamentarians.xlsx")
+
+# type in below all your commands
